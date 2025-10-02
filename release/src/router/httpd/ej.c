99878793677a95612e0330443a20d928f4acab56
黑猫 <133520@live.cn>
2025-10-02 23:50:15 +0800

    连我nn6000v2开机

--- release/src/router/httpd/ej.c
+++ release/src/router/httpd/ej.c
@@ -60,8 +60,8 @@ struct REPLACE_PRODUCTID_S replace_productid_t[] =
 	{"ZenWiFi_XD4", "灵耀AX魔方", "CN"},
 	{"ZenWiFi_CD6R", "ZenWiFi AC Mini", "global"},
 	{"ZenWiFi_CD6N", "ZenWiFi AC Mini", "global"},
-	{"ZenWiFi_XP4", "Link NN6000v1", "global"},
-	{"ZenWiFi_XP4", "连我 NN6000v1", "CN"},
+	{"ZenWiFi_XP4", "Link NN6000v2", "global"},
+	{"ZenWiFi_XP4", "连我 NN6000v2", "CN"},
 	{"ZenWiFi_CV4", "ZenWiFi Voice", "global"},
 	{NULL, NULL, NULL}
 };
