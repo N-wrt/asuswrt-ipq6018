60912c985ddbe308f67980e9cd1d1a21bf336db7
黑猫 <133520@live.cn>
2025-10-07 00:39:13 +0800

    修改固件打包名称

--- release/src/router/httpd/ej.c
+++ release/src/router/httpd/ej.c
@@ -61,7 +61,7 @@ struct REPLACE_PRODUCTID_S replace_productid_t[] =
 	{"ZenWiFi_CD6R", "ZenWiFi AC Mini", "global"},
 	{"ZenWiFi_CD6N", "ZenWiFi AC Mini", "global"},
 	{"ZenWiFi_XP4", "Redmi AX5", "global"},
-	{"ZenWiFi_XP4", "Redmi AX5", "CN"},
+	{"ZenWiFi_XP4", "红米 AX5", "CN"},
 	{"ZenWiFi_CV4", "ZenWiFi Voice", "global"},
 	{NULL, NULL, NULL}
 };
