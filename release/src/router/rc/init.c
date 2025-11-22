f404a53b52ca52c21ee5d1c846fc616b14c08b47
黑猫 <133520@live.cn>
2025-11-23 03:29:24 +0800

    同步v2的修改

--- release/src/router/rc/init.c
+++ release/src/router/rc/init.c
@@ -5903,7 +5903,7 @@ int init_nvram(void)
 		{ // XP4
 			/* NEED to sync with the default HwVer in flash image */
 			int xp4_old = (nvram_get_int("HwVer") < 1);
-		/* PLC on eth1 */
+		/* PLC on eth4 */
 		if (!strlen(nvram_safe_get("HwId"))) { // for old SR sample
 			_dprintf("!!!WARNING!!! No HwId found, setto A!!\n");
 			nvram_set("HwId", "A");
@@ -5911,7 +5911,7 @@ int init_nvram(void)
 		nvram_set("boardflags", "0x100"); // although it is not used in ralink driver, set for vlan
 		nvram_set("lan_ifname", "br0");
 		if (nvram_match("HwId", "A")) {
-			wan_ifaces[WAN_IFACE_ID] = "eth4";
+			wan_ifaces[WAN_IFACE_ID] = "eth1";  // eth4 -> eth1
 		} else {
 			wan_ifaces[WAN_IFACE_ID] = "eth2"; // LAN1 port, follow CD6N rule
 		}
@@ -5921,28 +5921,28 @@ int init_nvram(void)
 		if (nvram_match("HwId", "A")) {
 #ifdef RTCONFIG_DUALWAN
 			lan_1=NULL;
-			strcpy(lan_ifs,"eth3 eth2 eth1 eth0");
+			strcpy(lan_ifs,"eth3 eth2 eth4 eth0");  // eth1 -> eth4
 			if (sw_mode() == SW_MODE_ROUTER && get_wans_dualwan() & WANSCAP_LAN) {
 				memset(lan_ifs,0, sizeof(lan_ifs));
 				if (nvram_match("wans_lanport", "1")) //lan port1
 				{	
 					lan_1="eth2";
-					strcpy(lan_ifs,"eth3 eth1 eth0");
+					strcpy(lan_ifs,"eth3 eth4 eth0");  // eth1 -> eth4
 				}
 				else if (nvram_match("wans_lanport", "2")) //lan port2
 				{			
 					lan_1="eth3";
-					strcpy(lan_ifs,"eth2 eth1 eth0");
+					strcpy(lan_ifs,"eth2 eth4 eth0");  // eth1 -> eth4
 				}
 				else
 					_dprintf("error setting\n");
 			}
 			set_basic_ifname_vars(wan_ifaces, lan_ifs, wl_ifaces, "usb", NULL, NULL , lan_1, 0);
 #else				
-			set_basic_ifname_vars(wan_ifaces, "eth3 eth2 eth1 eth0", wl_ifaces, "usb", NULL, NULL, NULL, 0);
+			set_basic_ifname_vars(wan_ifaces, "eth3 eth2 eth4 eth0", wl_ifaces, "usb", NULL, NULL, NULL, 0);  // eth1 -> eth4
 #endif			
 		} else {
-			set_basic_ifname_vars(wan_ifaces, "eth3 eth1 eth0", wl_ifaces, NULL, NULL, NULL, NULL, 0);
+			set_basic_ifname_vars(wan_ifaces, "eth3 eth4 eth0", wl_ifaces, NULL, NULL, NULL, NULL, 0);  // eth1 -> eth4
 		}
 	    if (xp4_old)
 		nvram_unset("btn_rst_gpio");
@@ -5951,14 +5951,9 @@ int init_nvram(void)
 	    else
 		nvram_set_int("btn_rst_gpio", 34|GPIO_ACTIVE_LOW);
 		nvram_set_int("btn_wps_gpio", 9);
-		nvram_set_int("led_blue_gpio", 69);
-	    if (xp4_old) {
-		nvram_set_int("led_green_gpio", 33);
-		nvram_set_int("led_red_gpio", 32);
-	    } else {
-		nvram_set_int("led_green_gpio", 70);
-		nvram_set_int("led_red_gpio", 50);
-	    }
+		nvram_set_int("led_blue_gpio", 69|GPIO_ACTIVE_LOW);
+		nvram_set_int("led_green_gpio", 70|GPIO_ACTIVE_LOW);
+		nvram_set_int("led_red_gpio", 50|GPIO_ACTIVE_LOW);
 		nvram_set_int("btn_rst_gpio_1", 42);
 
 		/* enable bled */
@@ -6022,11 +6017,11 @@ int init_nvram(void)
 #if defined(RTCONFIG_AMAS) /* AMAS_ETHDETECT should be enabled */
 		if (nvram_match("HwId", "B"))
 			nvram_set("wired_ifnames", "");
-		else { // HwId A, XP4R only PLC(eth1) is dynamic WAN/LAN
+		else { // HwId A, XP4R only PLC(eth4) is dynamic WAN/LAN  // eth1 -> eth4
 			if (aimesh_re_node())
-				nvram_set("wired_ifnames", "eth2 eth3");
+				nvram_set("wired_ifnames", "eth2 eth3 eth0");
 			else
-				nvram_set("wired_ifnames", "eth2 eth3 eth1 eth0");
+				nvram_set("wired_ifnames", "eth2 eth3 eth4 eth0");  // eth1 -> eth4
 		}
 #endif
 
@@ -6037,8 +6032,8 @@ int init_nvram(void)
 				nvram_set("eth_ifnames", "");
 			} else {
 				nvram_set("wl1_channel", "36"); /* fixed channel speeds up sta1 connection */
-				nvram_set("eth_ifnames", " eth0 eth1 eth2 eth3"); /* PLC, LAN1, LAN2 */
-				nvram_set("amas_ethif_type", "4 4 4"); /* PLC, 1G, 1G */
+				nvram_set("eth_ifnames", "eth0 eth1 eth2 eth3"); /* PLC, LAN1, LAN2 */
+				nvram_set("amas_ethif_type", "4 4 4 4"); /* PLC, 1G, 1G */
 				nvram_set("eth_priority", "0 3 1" " 1 1 1" " 2 2 1"); /* PLC priority:3, LAN1:1, LAN2:2 */
 				nvram_set("sta_priority", "2 0 5 1" " 5 1 4 1"); /* 2G priority:5, 5G priority:4 */
 				nvram_set("sta_phy_ifnames", "sta1 sta0"); /* 2G name, 5G name */
@@ -6052,9 +6047,9 @@ int init_nvram(void)
 				_dprintf("[%s][%d] sw mode = %d, repeater=%d, ap= %d ",
 							__func__, __LINE__,
 							sw_mode(),SW_MODE_REPEATER,SW_MODE_AP);
-				nvram_set("eth_ifnames", " eth0 eth1 eth4"); /* PLC(eth1), normal WAN(eth4)*/
+				nvram_set("eth_ifnames", "eth0 eth1 eth4"); /* PLC(eth1), normal WAN(eth4)*/
 				nvram_set("fixed_eth_ifnames", "eth4"); /* XP4R WAN(eth4) only as wan */
-				nvram_set("amas_ethif_type", "4 4"); /* PLC, 1G */
+				nvram_set("amas_ethif_type", "4 4 4"); /* PLC, 1G */
 				nvram_set("eth_priority", "0 2 1" " 1 1 1"); /* PLC priority:2, WAN priority:1 */
 				nvram_set("sta_priority", "2 0 4 1" " 5 1 3 1"); /* 2G priority:4, 5G priority:3 */
 				nvram_set("sta_phy_ifnames", "sta1 sta0"); /* 2G name, 5G name */
@@ -6071,7 +6066,7 @@ int init_nvram(void)
 		}
 
 		/* interface name & type mapping for lldp */
-		nvram_set("amas_lldp_ifnames", " eth0 eth1 eth2 eth3"); /* PLC, LAN1, LAN2 */
+		nvram_set("amas_lldp_ifnames", "eth0 eth1 eth2 eth3"); /* PLC, LAN1, LAN2 */
 		nvram_set("amas_lldp_iftypes", "4 4 4 4"); /* PLC, 1G, 1G */
 #endif
 		} // XP4
--- release/src/router/shared/sysdeps/qca/qca.c
+++ release/src/router/shared/sysdeps/qca/qca.c
@@ -1096,7 +1096,7 @@ static const char *query_ifname[PORT_UNITS] = { //Aimesh RE
 	NULL,   "vlan1",NULL,   NULL,   "vlan4",NULL
 #elif defined(PLAX56_XP4)
 //	P0	P1	P2	P3	P4	P5
-	"eth0",	"eth1",	"eth2",	"eth3",	"eth4",	"eth5"
+	"eth0",	"eth2",	"eth3",	"eth4",	"eth1",	NULL
 #else
 //	P0	P1	P2	P3	P4	P5
 	NULL,   NULL,   NULL,   NULL,   NULL,   NULL
